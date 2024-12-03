e100d32d49c6d1c6fdc95101fb378524ff268dcf
ShevchenkoYuliia <yuliia.shevchenko1845@gmail.com>
2024-12-03 15:13:13 +0200

    exam

--- exam/hash_json_converter.rb
+++ exam/hash_json_converter.rb
@@ -2,10 +2,16 @@ require 'json'
 
 def hash_to_json(hash)
   hash.to_json
+rescue => e
+  puts "Error converting hash to JSON: #{e.message}"
+  nil
 end
 
 def json_to_hash(json_string)
   JSON.parse(json_string)
+rescue JSON::ParserError => e
+  puts "Error parsing JSON string: #{e.message}"
+  nil
 end
 
 hash_example = { name: "Jack", age: 30, skills: ["Ruby", "JavaScript"] }
@@ -20,3 +26,9 @@ puts json_result
 hash_result = json_to_hash(json_result)
 puts "\nJSON to Hash:"
 p hash_result
+
+puts "\nInvalid JSON:"
+invalid_json = '{"name": "Jack", "age": 30, "skills": ["Ruby", "JavaScript"'
+hash_result1 = json_to_hash(invalid_json)
+puts "\nJSON to Hash:"
+p hash_result1
